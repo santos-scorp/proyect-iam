1f4d27c77aaab678ba3e477f04b06d5468880434
santos-scorp <pedroernestosantosdiaz@gmail.com>
2024-11-05 07:31:32 -0600

    Configuracion de ip staticas en docker

--- Frontend/src/services/config.js
+++ Frontend/src/services/config.js
@@ -1,5 +1,5 @@
 const configService = {
-    apiUrl : 'http://localhost:4848/api',
-    urlStatic: 'http://localhost:4848/static'
+    apiUrl : 'http://10.5.0.5:4848/api',
+    urlStatic: 'http://10.5.0.5:4848/static'
 }
 export default configService
